84f5cf64be9da2fcf79b173735b4c7a046d5b942
Frost Sheridan <acrennie@gmail.com>
2020-10-25 15:37:27 -0400

    wow first commit, cool

---
+++ AudioSnippingTool/Form1.Designer.cs
@@ -0,0 +1,229 @@
+﻿namespace AudioSnippingTool
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.toolStripStatusLabel_status = new System.Windows.Forms.ToolStripStatusLabel();
+            this.toolStrip1 = new System.Windows.Forms.ToolStrip();
+            this.toolStripButton_record = new System.Windows.Forms.ToolStripButton();
+            this.toolStripButton_stop = new System.Windows.Forms.ToolStripButton();
+            this.toolStripButton_play = new System.Windows.Forms.ToolStripButton();
+            this.toolStripSeparator1 = new System.Windows.Forms.ToolStripSeparator();
+            this.toolStripLabel1 = new System.Windows.Forms.ToolStripLabel();
+            this.toolStripButton_wav = new System.Windows.Forms.ToolStripButton();
+            this.toolStripButton_mp3 = new System.Windows.Forms.ToolStripButton();
+            this.toolStripButton_export = new System.Windows.Forms.ToolStripButton();
+            this.timer_updateplaydisp = new System.Windows.Forms.Timer(this.components);
+            this.pictureBox_wave = new System.Windows.Forms.PictureBox();
+            this.statusStrip1.SuspendLayout();
+            this.toolStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox_wave)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripStatusLabel_status});
+            this.statusStrip1.Location = new System.Drawing.Point(0, 187);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Size = new System.Drawing.Size(516, 22);
+            this.statusStrip1.SizingGrip = false;
+            this.statusStrip1.TabIndex = 0;
+            this.statusStrip1.Text = "statusStrip1";
+            // 
+            // toolStripStatusLabel_status
+            // 
+            this.toolStripStatusLabel_status.Name = "toolStripStatusLabel_status";
+            this.toolStripStatusLabel_status.Size = new System.Drawing.Size(223, 17);
+            this.toolStripStatusLabel_status.Text = "Audio Snipping Tool - by Frost Sheridan";
+            // 
+            // toolStrip1
+            // 
+            this.toolStrip1.GripStyle = System.Windows.Forms.ToolStripGripStyle.Hidden;
+            this.toolStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripButton_record,
+            this.toolStripButton_stop,
+            this.toolStripButton_play,
+            this.toolStripSeparator1,
+            this.toolStripLabel1,
+            this.toolStripButton_wav,
+            this.toolStripButton_mp3,
+            this.toolStripButton_export});
+            this.toolStrip1.Location = new System.Drawing.Point(0, 0);
+            this.toolStrip1.Name = "toolStrip1";
+            this.toolStrip1.Size = new System.Drawing.Size(516, 31);
+            this.toolStrip1.TabIndex = 2;
+            this.toolStrip1.Text = "toolStrip1";
+            // 
+            // toolStripButton_record
+            // 
+            this.toolStripButton_record.Image = global::AudioSnippingTool.Properties.Resources.icon_record;
+            this.toolStripButton_record.ImageScaling = System.Windows.Forms.ToolStripItemImageScaling.None;
+            this.toolStripButton_record.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.toolStripButton_record.Name = "toolStripButton_record";
+            this.toolStripButton_record.Size = new System.Drawing.Size(102, 28);
+            this.toolStripButton_record.Text = "Record New";
+            this.toolStripButton_record.ToolTipText = "Start recording a new audio snippet";
+            this.toolStripButton_record.Click += new System.EventHandler(this.toolStripButton_record_Click);
+            // 
+            // toolStripButton_stop
+            // 
+            this.toolStripButton_stop.Enabled = false;
+            this.toolStripButton_stop.Image = global::AudioSnippingTool.Properties.Resources.icon_stop;
+            this.toolStripButton_stop.ImageScaling = System.Windows.Forms.ToolStripItemImageScaling.None;
+            this.toolStripButton_stop.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.toolStripButton_stop.Name = "toolStripButton_stop";
+            this.toolStripButton_stop.Size = new System.Drawing.Size(61, 28);
+            this.toolStripButton_stop.Text = "Stop";
+            this.toolStripButton_stop.ToolTipText = "End recording or stop listening";
+            this.toolStripButton_stop.Click += new System.EventHandler(this.toolStripButton_stop_Click);
+            // 
+            // toolStripButton_play
+            // 
+            this.toolStripButton_play.Enabled = false;
+            this.toolStripButton_play.Image = global::AudioSnippingTool.Properties.Resources.icon_play;
+            this.toolStripButton_play.ImageScaling = System.Windows.Forms.ToolStripItemImageScaling.None;
+            this.toolStripButton_play.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.toolStripButton_play.Name = "toolStripButton_play";
+            this.toolStripButton_play.Size = new System.Drawing.Size(67, 28);
+            this.toolStripButton_play.Text = "Listen";
+            this.toolStripButton_play.ToolTipText = "Play recorded audio";
+            this.toolStripButton_play.Click += new System.EventHandler(this.toolStripButton_play_Click);
+            // 
+            // toolStripSeparator1
+            // 
+            this.toolStripSeparator1.Name = "toolStripSeparator1";
+            this.toolStripSeparator1.Size = new System.Drawing.Size(6, 31);
+            // 
+            // toolStripLabel1
+            // 
+            this.toolStripLabel1.Name = "toolStripLabel1";
+            this.toolStripLabel1.Size = new System.Drawing.Size(62, 28);
+            this.toolStripLabel1.Text = "Export as:";
+            // 
+            // toolStripButton_wav
+            // 
+            this.toolStripButton_wav.Checked = true;
+            this.toolStripButton_wav.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.toolStripButton_wav.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Text;
+            this.toolStripButton_wav.Image = ((System.Drawing.Image)(resources.GetObject("toolStripButton_wav.Image")));
+            this.toolStripButton_wav.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.toolStripButton_wav.Name = "toolStripButton_wav";
+            this.toolStripButton_wav.Size = new System.Drawing.Size(39, 28);
+            this.toolStripButton_wav.Text = "WAV";
+            this.toolStripButton_wav.ToolTipText = "Recording will be exported as a .wav file";
+            this.toolStripButton_wav.Click += new System.EventHandler(this.toolStripButton_wav_Click);
+            // 
+            // toolStripButton_mp3
+            // 
+            this.toolStripButton_mp3.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Text;
+            this.toolStripButton_mp3.Image = ((System.Drawing.Image)(resources.GetObject("toolStripButton_mp3.Image")));
+            this.toolStripButton_mp3.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.toolStripButton_mp3.Name = "toolStripButton_mp3";
+            this.toolStripButton_mp3.Size = new System.Drawing.Size(34, 28);
+            this.toolStripButton_mp3.Text = "MP3";
+            this.toolStripButton_mp3.ToolTipText = "Recording will be exported as a 320kbps .mp3 file";
+            this.toolStripButton_mp3.Click += new System.EventHandler(this.toolStripButton_mp3_Click);
+            // 
+            // toolStripButton_export
+            // 
+            this.toolStripButton_export.Alignment = System.Windows.Forms.ToolStripItemAlignment.Right;
+            this.toolStripButton_export.Enabled = false;
+            this.toolStripButton_export.Image = global::AudioSnippingTool.Properties.Resources.icon_export;
+            this.toolStripButton_export.ImageScaling = System.Windows.Forms.ToolStripItemImageScaling.None;
+            this.toolStripButton_export.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.toolStripButton_export.Name = "toolStripButton_export";
+            this.toolStripButton_export.Size = new System.Drawing.Size(71, 28);
+            this.toolStripButton_export.Text = "Export";
+            this.toolStripButton_export.ToolTipText = "Drag me to another window to export audio";
+            this.toolStripButton_export.MouseDown += new System.Windows.Forms.MouseEventHandler(this.toolStripButton_export_MouseDown);
+            // 
+            // timer_updateplaydisp
+            // 
+            this.timer_updateplaydisp.Interval = 10;
+            this.timer_updateplaydisp.Tick += new System.EventHandler(this.timer_updateplaydisp_Tick);
+            // 
+            // pictureBox_wave
+            // 
+            this.pictureBox_wave.Cursor = System.Windows.Forms.Cursors.IBeam;
+            this.pictureBox_wave.Location = new System.Drawing.Point(8, 34);
+            this.pictureBox_wave.Name = "pictureBox_wave";
+            this.pictureBox_wave.Size = new System.Drawing.Size(500, 150);
+            this.pictureBox_wave.TabIndex = 1;
+            this.pictureBox_wave.TabStop = false;
+            this.pictureBox_wave.Paint += new System.Windows.Forms.PaintEventHandler(this.pictureBox_wave_Paint);
+            this.pictureBox_wave.MouseDown += new System.Windows.Forms.MouseEventHandler(this.pictureBox_wave_MouseDown);
+            this.pictureBox_wave.MouseMove += new System.Windows.Forms.MouseEventHandler(this.pictureBox_wave_MouseMove);
+            this.pictureBox_wave.MouseUp += new System.Windows.Forms.MouseEventHandler(this.pictureBox_wave_MouseUp);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(516, 209);
+            this.Controls.Add(this.toolStrip1);
+            this.Controls.Add(this.pictureBox_wave);
+            this.Controls.Add(this.statusStrip1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.SizeGripStyle = System.Windows.Forms.SizeGripStyle.Hide;
+            this.Text = "Audio Snipping Tool";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.statusStrip1.ResumeLayout(false);
+            this.statusStrip1.PerformLayout();
+            this.toolStrip1.ResumeLayout(false);
+            this.toolStrip1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox_wave)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabel_status;
+        private System.Windows.Forms.PictureBox pictureBox_wave;
+        private System.Windows.Forms.ToolStrip toolStrip1;
+        private System.Windows.Forms.ToolStripButton toolStripButton_record;
+        private System.Windows.Forms.ToolStripButton toolStripButton_stop;
+        private System.Windows.Forms.ToolStripButton toolStripButton_play;
+        private System.Windows.Forms.ToolStripSeparator toolStripSeparator1;
+        private System.Windows.Forms.ToolStripLabel toolStripLabel1;
+        private System.Windows.Forms.ToolStripButton toolStripButton_wav;
+        private System.Windows.Forms.ToolStripButton toolStripButton_mp3;
+        private System.Windows.Forms.Timer timer_updateplaydisp;
+        private System.Windows.Forms.ToolStripButton toolStripButton_export;
+    }
+}
+
---
+++ AudioSnippingTool/Form1.cs
@@ -0,0 +1,362 @@
+﻿using NAudio.Wave;
+using NAudio.Lame;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Runtime.CompilerServices;
+
+namespace AudioSnippingTool
+{
+    public partial class Form1 : Form
+    {
+        private readonly string filename_wav = "AST_Raw.wav";
+        private readonly string filename_trim_wav = "AST_Export.wav";
+        private readonly string filename_trim_mp3 = "AST_Export.mp3";
+
+        WasapiLoopbackCapture audio_capture;
+        WaveFileWriter audio_writer;
+        WaveFileReader audio_player_reader;
+        WaveOut audio_player;
+
+        bool file_ready = false;
+        bool file_playing = false;
+        bool selecting = false;
+        long selection_start = 0, selection_end = 0, num_audio_blocks = 0;
+        float recording_seconds = 0;
+        Bitmap waveform_bmp;
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            //create bmp for drawing waveform display onto
+            waveform_bmp = new Bitmap(pictureBox_wave.Width, pictureBox_wave.Height);
+        }
+
+        void showSplashScreen()
+        {
+            toolStripStatusLabel_status.Text = "Audio Snipping Tool - by Frost Sheridan";
+            pictureBox_wave.Refresh();
+        }
+
+        //======================================================================================================
+        //audio events
+        //======================================================================================================
+
+        //new data is available while recording, so save it to the files
+        private void audioDataAvailable(object sender, WaveInEventArgs e)
+        {
+            audio_writer.Write(e.Buffer, 0, e.BytesRecorded);
+
+            recording_seconds = (float)audio_writer.Length / audio_writer.WaveFormat.AverageBytesPerSecond;
+            toolStripStatusLabel_status.Text = "Now recording... " + recording_seconds.ToString("0.0") + " sec";
+        }
+
+        //recording has been stopped by the user
+        private void audioRecordingStopped(object sender, StoppedEventArgs e)
+        {
+            selection_start = 0;
+            num_audio_blocks = audio_writer.Length / audio_writer.WaveFormat.BlockAlign;
+            selection_end = num_audio_blocks - 1;
+            audio_writer.Dispose();
+            audio_capture.Dispose();
+
+            if (audio_writer.Length == 0)
+            {
+                toolStripButton_record.Enabled = true;
+                toolStripButton_stop.Enabled = false;
+                toolStripButton_play.Enabled = false;
+                toolStripButton_export.Enabled = false;
+                file_ready = false;
+                showSplashScreen();
+            }
+            else
+            {
+                //update UI
+                toolStripButton_record.Enabled = true;
+                toolStripButton_stop.Enabled = false;
+                toolStripButton_play.Enabled = true;
+                toolStripButton_export.Enabled = true;
+                file_ready = true;
+
+                renderWaveformDisplay();
+                saveTrimmedAudio();
+            }
+        }
+
+        //playback has completed or has been stopped by the user
+        void audioPlaybackStopped(object sender, StoppedEventArgs e)
+        {
+            audio_player_reader.Dispose();
+            audio_player.Dispose();
+
+            //update UI
+            timer_updateplaydisp.Enabled = false;
+            toolStripButton_record.Enabled = true;
+            toolStripButton_stop.Enabled = false;
+            toolStripButton_play.Enabled = true;
+            toolStripButton_export.Enabled = true;
+            file_playing = false;
+            pictureBox_wave.Refresh();
+        }
+
+
+        //======================================================================================================
+        //ui element events
+        //======================================================================================================
+
+        private void toolStripButton_record_Click(object sender, EventArgs e)
+        {
+            //update UI
+            toolStripButton_record.Enabled = false;
+            toolStripButton_stop.Enabled = true;
+            toolStripButton_play.Enabled = false;
+            toolStripButton_export.Enabled = false;
+            toolStripStatusLabel_status.Text = "Now recording...";
+            file_ready = false;
+            pictureBox_wave.Refresh();
+
+            //create WASAPI recording and saving objects
+            audio_capture = new WasapiLoopbackCapture();
+            audio_writer = new WaveFileWriter(filename_wav, audio_capture.WaveFormat);
+
+            //when audio has been captured, save it to the file
+            audio_capture.DataAvailable += audioDataAvailable;
+
+            //when audio recording has stopped, close the file and get rid of recording objects
+            audio_capture.RecordingStopped += audioRecordingStopped;
+
+            //start recording audio
+            audio_capture.StartRecording();
+        }
+
+        private void toolStripButton_stop_Click(object sender, EventArgs e)
+        {
+            if(file_playing)
+            {
+                audio_player.Stop();
+            }
+            else
+            {
+                //stop recording audio and save file
+                audio_capture.StopRecording();
+            }
+        }
+
+        private void toolStripButton_play_Click(object sender, EventArgs e)
+        {
+            //update UI
+            toolStripButton_record.Enabled = false;
+            toolStripButton_stop.Enabled = true;
+            toolStripButton_play.Enabled = false;
+            toolStripButton_export.Enabled = false;
+            file_playing = true;
+            timer_updateplaydisp.Enabled = true;
+
+            //play file
+            audio_player_reader = new WaveFileReader(filename_trim_wav);
+            audio_player = new WaveOut();
+            audio_player.Init(audio_player_reader);
+            audio_player.Play();
+            audio_player.PlaybackStopped += audioPlaybackStopped;
+        }
+
+        private void pictureBox_wave_MouseDown(object sender, MouseEventArgs e)
+        {
+            if(file_ready && !file_playing)
+            {
+                if(e.Button == MouseButtons.Right)
+                {
+                    selection_start = 0;
+                    selection_end = num_audio_blocks - 1;
+                    pictureBox_wave.Refresh();
+                    saveTrimmedAudio();
+                }
+                else
+                {
+                    selecting = true;
+                    selection_end = selection_start = mouseXToBlock(e.Location.X);
+                    pictureBox_wave.Refresh();
+                }
+            }
+        }
+
+        private void pictureBox_wave_MouseMove(object sender, MouseEventArgs e)
+        {
+            if(selecting)
+            {
+                selection_end = mouseXToBlock(e.Location.X);
+                pictureBox_wave.Refresh();
+            }
+        }
+
+        private void pictureBox_wave_MouseUp(object sender, MouseEventArgs e)
+        {
+            if(selecting && e.Button == MouseButtons.Left)
+            {
+                selecting = false;
+                if (selection_end < selection_start)
+                {
+                    long temp = selection_start;
+                    selection_start = selection_end;
+                    selection_end = temp;
+                }
+                if (selection_end - selection_start == 0)
+                {
+                    selection_start = 0;
+                    selection_end = num_audio_blocks - 1;
+                    pictureBox_wave.Refresh();
+                }
+                saveTrimmedAudio();
+            }
+        }
+        
+
+        private void toolStripButton_wav_Click(object sender, EventArgs e)
+        {
+            toolStripButton_wav.Checked = true;
+            toolStripButton_mp3.Checked = false;
+        }
+
+        private void toolStripButton_mp3_Click(object sender, EventArgs e)
+        {
+            toolStripButton_mp3.Checked = true;
+            toolStripButton_wav.Checked = false;
+        }
+
+        private void toolStripButton_export_MouseDown(object sender, MouseEventArgs e)
+        {
+            if (file_ready)
+            {
+                FileInfo fi;
+                if (toolStripButton_wav.Checked)
+                {
+                    fi = new FileInfo(filename_trim_wav);
+                }
+                else
+                {
+                    fi = new FileInfo(filename_trim_mp3);
+                }
+
+                string[] filename_to_drag = { fi.FullName };
+                toolStripButton_export.DoDragDrop(new DataObject(DataFormats.FileDrop, filename_to_drag), DragDropEffects.Copy);
+            }
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            deleteFileIfExists(filename_wav);
+            deleteFileIfExists(filename_trim_wav);
+            deleteFileIfExists(filename_trim_mp3);
+        }
+        void deleteFileIfExists(string filename)
+        {
+            if(new FileInfo(filename).Exists)
+            {
+                File.Delete(filename);
+            }
+        }
+        void saveTrimmedAudio()
+        {
+            WaveFileReader reader = new WaveFileReader(filename_wav);
+            WaveFileWriter writer_wav = new WaveFileWriter(filename_trim_wav, reader.WaveFormat);
+            LameMP3FileWriter writer_mp3 = new LameMP3FileWriter(filename_trim_mp3, reader.WaveFormat, 320);
+
+            reader.Position = selection_start * reader.WaveFormat.BlockAlign;
+            long end_bytes = selection_end * reader.WaveFormat.BlockAlign;
+
+            byte[] buf = new byte[reader.WaveFormat.BlockAlign * (selection_end - selection_start)];
+            reader.Read(buf, 0, buf.Length);
+            writer_mp3.Write(buf, 0, buf.Length);
+            writer_wav.Write(buf, 0, buf.Length);
+
+            reader.Dispose();
+            writer_mp3.Dispose();
+            writer_wav.Dispose();
+
+            recording_seconds = (float)writer_wav.Length / writer_wav.WaveFormat.AverageBytesPerSecond;
+            double file_mb_wav = new FileInfo(filename_trim_wav).Length / 1024.0 / 1024.0;
+            double file_mb_mp3 = new FileInfo(filename_trim_mp3).Length / 1024.0 / 1024.0;
+            toolStripStatusLabel_status.Text = "Recording complete. " + recording_seconds.ToString("0.0") + " sec | WAV size: " + file_mb_wav.ToString("0.00") + " MB | MP3 size: " + file_mb_mp3.ToString("0.00") + " MB";
+        }
+
+        //======================================================================================================
+        //draw waveform display
+        //======================================================================================================
+
+        private void pictureBox_wave_Paint(object sender, PaintEventArgs e)
+        {
+            Graphics g = e.Graphics;
+            g.Clear(Color.Black);
+            if(file_ready)
+            {
+                float rect_x = map((selection_end >= selection_start) ? selection_start : selection_end, 0, num_audio_blocks, 0, pictureBox_wave.Width);
+                float rect_w = map(Math.Abs(selection_end - selection_start), 0, num_audio_blocks - 1, 0, pictureBox_wave.Width);
+                SolidBrush b = new SolidBrush(Color.FromArgb(30,0,70));
+                g.FillRectangle(b, rect_x, 0, rect_w, pictureBox_wave.Height);
+
+                if(file_playing)
+                {
+                    rect_w = map(audio_player.GetPosition(), 0, audio_player_reader.Length, 0, rect_w);
+                    b = new SolidBrush(Color.FromArgb(0,150,0));
+                    g.FillRectangle(b, rect_x, 0, rect_w, pictureBox_wave.Height);
+                }
+
+                g.DrawImage(waveform_bmp,0,0,pictureBox_wave.Width, pictureBox_wave.Height);
+            }
+            else
+            {
+                g.DrawImage(Properties.Resources.splash,0,0,pictureBox_wave.Width,pictureBox_wave.Height);
+            }
+        }
+
+        void renderWaveformDisplay()
+        {
+            using (Graphics g = Graphics.FromImage(waveform_bmp))
+            {
+                g.Clear(Color.Transparent);
+
+                AudioFileReader wav_file = new AudioFileReader(filename_wav);
+                float[] samples = new float[wav_file.Length / wav_file.BlockAlign * 2];
+                wav_file.Read(samples, 0, samples.Length);
+                wav_file.Dispose();
+                
+                for(int x = 0; x < waveform_bmp.Width; x++)
+                {
+                    int audio_pos = (int)map(x, 0, waveform_bmp.Width, 0, samples.Length / 2) * 2;
+                    float y_left = map(Math.Abs(samples[audio_pos]), 0, 1, waveform_bmp.Height / 2, 0);
+                    float y_right = map(Math.Abs(samples[audio_pos + 1]), 0, 1, waveform_bmp.Height / 2, waveform_bmp.Height - 1);
+                    g.DrawLine(Pens.Cyan, x, y_left, x, y_right);
+                }
+            }
+            pictureBox_wave.Refresh();
+        }
+
+        private void timer_updateplaydisp_Tick(object sender, EventArgs e)
+        {
+            pictureBox_wave.Refresh();
+        }
+
+        float map(float x, float in_min, float in_max, float out_min, float out_max)
+        {
+            return (x - in_min) * (out_max - out_min) / (in_max - in_min) + out_min;
+        }
+
+        long maplong(long x, long in_min, long in_max, long out_min, long out_max)
+        {
+            return (x - in_min) * (out_max - out_min) / (in_max - in_min) + out_min;
+        }
+
+        long mouseXToBlock(int mouse_x)
+        {
+            return maplong(mouse_x, 0, pictureBox_wave.Width, 0, num_audio_blocks);
+        }
+    }
+}
